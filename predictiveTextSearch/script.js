75c02e40b1e5aed22800c76e9fec2a497ff4d9ea
Paul Mozeley <pdreier15@gmail.com>
2024-06-07 17:22:54 -0400

    bolding letters that are included within input value

--- predictiveTextSearch/script.js
+++ predictiveTextSearch/script.js
@@ -1,6 +1,6 @@
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('ul');
-const li = document.querySelectorAll('li');
+
 
 const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
@@ -9,9 +9,7 @@ it is then added to a new array called 'results' */
 function search(str) {
 	/* create new empty array */
 	let results = [];
-	/* assigning str to the current input value */
-	str = input.value;
-	
+
 	/* for each fruit in fruits array, if fruit(lowecase to avoid Case sensitive errors) includes 
 	the input value anywhere in it. push that fruit to the new array 'results' */
 	fruits.forEach(fruit => {
@@ -35,24 +33,37 @@ function searchHandler(e) {
 	not seem to be part of the step list, but I noticed the example was doing this and wanted to
 	see how it was done. */
 	inputVal = input.value;
-	e = search();
+	/* declaring results as the return value of search */
+	const results = search(inputVal);
+	/* putting values into showSuggestions function */
+	/* if input includes char from fruit. bold char from fruit */
+	
+	showSuggestions(results,inputVal);
+
 	
 	
 }
 
 /* creating an li out of each fruit from the results array created in the search() function
 and appending that li to the 'suggestions' ul.*/
-function showSuggestions(results) {
-	/* declaring results as the return value from search(); */
-	results = search();
+function showSuggestions(results, inputVal) {
 	/* making sure that the suggestions ul is empty before appending each fruit(li); */
 	suggestions.innerHTML = '';
 	/* for each fruit in results, create an li called fruitLi. assign the text content of that fruit
 	to the newly created fruitLi. Then append fruitLi to suggestions. */
 	results.forEach(fruit => {
 		const fruitLi = document.createElement('li');
-		fruitLi.textContent = fruit;
-		suggestions.appendChild(fruitLi)
+		/* if first half or ternary operator is 'truthy' bold fruit, if not just print fruit */
+		/* if */
+		let boldedFruit = '';
+		for(let i=0; i<fruit.length; i++){
+
+			inputVal.includes(fruit[i]) ? boldedFruit += `<strong>${fruit[i]}</strong>` : boldedFruit += fruit[i];
+
+		}
+
+		fruitLi.innerHTML = boldedFruit;
+		suggestions.appendChild(fruitLi);
 	});
 
 }
@@ -61,11 +72,13 @@ function showSuggestions(results) {
 current input value to that targets textContent. */
 function useSuggestion(e) {
 	input.value = e.target.textContent;	
+	/* empty ul */
+	suggestions.innerHTML = '';
 }
 
 
 
 
-input.addEventListener('keyup',showSuggestions)
+
 input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
